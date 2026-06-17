32d4321548548fb27a52d79b34ca3d7b7e6c74e2
WernerDJ <wernerdjuan@gmail.com>
2026-06-17 12:54:31 +0200

    Add migration to fix Ingredient OneToOne to ForeignKey

---
+++ backend/api/migrations/0002_fix_ingredient_fk.py
@@ -0,0 +1,25 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='ingredient',
+            name='product',
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='ingredients',
+                to='api.product'
+            ),
+        ),
+        migrations.AlterUniqueTogether(
+            name='ingredient',
+            unique_together={('product', 'source')},
+        ),
+    ]
